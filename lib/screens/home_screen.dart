33d3ae2d39217ce9d0ffbe74f99187f47b562c09
julio <dev.julio.furtado@gmail.com>
2022-03-01 01:19:04 -0300

    initiated adjustments for different screen sizes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_interface/utils/responsive.dart';
 import 'package:facebook_interface/widgets/create_post_area_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
@@ -20,62 +21,112 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            floating: true,
-            centerTitle: false,
-            title: Text(
-              "facebook",
-              style: TextStyle(
-                color: ColorPallete.blueFacebook,
-                fontWeight: FontWeight.bold,
-                fontSize: 28,
-                letterSpacing: -1.2,
-              ),
-            ),
-            actions: [
-              ButtonCircleWidget(
-                icon: Icons.search,
-                iconSize: 30,
-                onPressed: () {},
-              ),
-              ButtonCircleWidget(
-                icon: LineIcons.facebookMessenger,
-                iconSize: 30,
-                onPressed: () {},
-              ),
-            ],
-          ),
-          SliverToBoxAdapter(
-            child: CreatePostAreaWidget(
-              user: currentUser,
-            ),
-          ),
-          SliverPadding(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
-            sliver: SliverToBoxAdapter(
-              child: StoryAreaWidget(
-                user: currentUser,
-                storysList: storysList,
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                Post post = posts[index];
-
-                return PostCardWidget(
-                  post: post,
-                );
-              },
-              childCount: posts.length,
-            ),
-          ),
-        ],
+      body: Responsive(
+        mobile: HomeMobile(),
+        desktop: HomeDesktop(),
       ),
+    );
+  }
+}
+
+class HomeMobile extends StatelessWidget {
+  const HomeMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          floating: true,
+          centerTitle: false,
+          title: Text(
+            "facebook",
+            style: TextStyle(
+              color: ColorPallete.blueFacebook,
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              letterSpacing: -1.2,
+            ),
+          ),
+          actions: [
+            ButtonCircleWidget(
+              icon: Icons.search,
+              iconSize: 30,
+              onPressed: () {},
+            ),
+            ButtonCircleWidget(
+              icon: LineIcons.facebookMessenger,
+              iconSize: 30,
+              onPressed: () {},
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: CreatePostAreaWidget(
+            user: currentUser,
+          ),
+        ),
+        SliverPadding(
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: StoryAreaWidget(
+              user: currentUser,
+              storysList: storysList,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              Post post = posts[index];
+
+              return PostCardWidget(
+                post: post,
+              );
+            },
+            childCount: posts.length,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeDesktop extends StatelessWidget {
+  const HomeDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: StoryAreaWidget(
+              user: currentUser,
+              storysList: storysList,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: CreatePostAreaWidget(
+            user: currentUser,
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              Post post = posts[index];
+
+              return PostCardWidget(
+                post: post,
+              );
+            },
+            childCount: posts.length,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/utils/responsive.dart
+++ lib/utils/responsive.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Responsive extends StatelessWidget {
-  const Responsive({Key? key}) : super(key: key);
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget desktop;
+
+  const Responsive(
+      {Key? key, required this.mobile, required this.desktop, this.tablet})
+      : super(key: key);
 
   static bool isMobile(BuildContext context) {
     return MediaQuery.of(context).size.width < 800;
@@ -18,6 +24,18 @@ class Responsive extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth >= 1200) {
+        return desktop;
+      } else if (constraints.maxWidth >= 800) {
+        Widget? restablet = this.tablet;
+        if (restablet != null) {
+          return restablet;
+        }
+        return this.mobile;
+      } else {
+        return mobile;
+      }
+    });
   }
 }
